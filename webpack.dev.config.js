c6d72f33cbd7ad3f702d39f1db9ab2c1936c217a
Christian Rank <git@christianrank.de>
2017-02-11 17:43:12 +0100

    fixed lint errors, removed config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -4,35 +4,34 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const path              = require('path');
 
 module.exports = {
-    // entry: ['webpack/hot/dev-server', './src/js/app.js'],
     entry: './src/js/app.js',
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'app.js',
     },
     module: {
         rules: [
             {
                 test: /\.(css|less)$/,
                 loader: ExtractTextPlugin.extract({
-                  loader: [
-                      {
-                          loader: 'css-loader',
-                          options: {
-                              modules: true,
-                              importLoaders: true,
-                              localIdentName: "[name]__[local]___[hash:base64:5]",
-                          }
-                      },
-                      'less-loader',
-                      'postcss-loader',
-                  ]
-                })
+                    loader: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                importLoaders: true,
+                                localIdentName: '[name]__[local]___[hash:base64:5]',
+                            },
+                        },
+                        'less-loader',
+                        'postcss-loader',
+                    ],
+                }),
             },
             {
                 test: /\.(jpg|gif|png|webp|ttf|eot|svg|woff(2)?)$/,
                 use: [
-                    'file-loader'
+                    'file-loader',
                 ],
             },
             {
@@ -43,12 +42,12 @@ module.exports = {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['es2015', 'stage-0', 'react']
-                        }
-                    }
+                            presets: ['es2015', 'stage-0', 'react'],
+                        },
+                    },
                 ],
             },
-        ]
+        ],
     },
     devtool: 'eval-source-map',
     plugins: [
@@ -58,8 +57,8 @@ module.exports = {
         }),
         new webpack.LoaderOptionsPlugin({
             options: {
-                postcss: [autoprefixer]
-            }
+                postcss: [autoprefixer],
+            },
         }),
         new webpack.DefinePlugin({
             'process.env': {
@@ -73,14 +72,11 @@ module.exports = {
             path.join(__dirname, 'src/js'),
             'node_modules',
         ],
-        alias: {
-            config: path.join(__dirname, 'src/js/config/development.js'),
-        },
     },
     // exclude and load from cdnjs
     externals: {
-        //jquery: 'jQuery',
-        //react: 'React',
-        //'react-dom': 'ReactDOM'
-    }
+        // jquery: 'jQuery',
+        // react: 'React',
+        // 'react-dom': 'ReactDOM'
+    },
 };
